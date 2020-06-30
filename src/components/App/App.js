39b9f10e04e374c7f91d4caece055e61d9ab63d3
OUARRICH <warichsaid@gmail.com>
2020-06-30 20:16:57 +0200

    module2

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,10 +1,30 @@
 import React, { Component } from 'react';
 import OptionsPanel from '../OptionsPanel'
 import Board from '../Board'
+import {createTiles} from '../../misc/utils'
 
 import './App.css';
 
 class App extends Component{
+  constructor(props) {
+    super(props);
+    this.state = {
+      numTiles: 36,
+      previousTileIndex: null,
+      playing: false,
+      toBeCleared: null,
+      tiles: []
+    };
+  }
+
+  startGame = () => {
+    this.setState(state => ({
+      playing: true,
+      previousTileIndex: null,
+      toBeCleared: null,
+      tiles: createTiles(this.state.numTiles)
+    }));
+  }
 
   render() {
   return (
@@ -12,8 +32,12 @@ class App extends Component{
       <header className="App-header">
         Turbo-Matcher
       </header>
-        <OptionsPanel />
-        <Board />
+        <OptionsPanel 
+          numTiles={this.state.numTiles}
+          playing={this.state.playing}
+          startGame={this.startGame}
+        />
+        <Board numTiles={this.state.numTiles} tiles={this.state.tiles}/>
       }
     </div>
   );
